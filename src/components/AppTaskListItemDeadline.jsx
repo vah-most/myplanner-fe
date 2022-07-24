6d1b2ed6d5c0fca176b6c3f992881a7bf464d56d
Mostafa Vahabzadeh <vah.most.85@gmail.com>
2022-07-24 13:02:56 +0430

    Changed date-picker third-party component to resolve version-conflict issues with react@18

--- src/components/AppTaskListItemDeadline.jsx
+++ src/components/AppTaskListItemDeadline.jsx
@@ -7,42 +7,21 @@
  */
 
 import React from "react";
-import Datetime from "react-datetime";
 import { now } from "moment";
 
 import { getRemainingDays } from "../utils/utils";
 
 import AppIcon from "./common/AppIcon";
 
-import "react-datetime/css/react-datetime.css";
 import "./AppTaskListItemDeadline.scss";
 
-const AppTaskListItemDeadline = ({
-  deadline,
-  onTaskDeadlineChange,
-  taskId,
-}) => {
+const AppTaskListItemDeadline = ({ deadline }) => {
   let due = null;
   if (deadline) {
     const currTime = now();
     due = new Date(deadline).getTime();
     due = getRemainingDays(currTime, due);
   }
-
-  const renderTaskDeadlineCalendar = (props, openCalendar, closeCalendar) => {
-    function clear() {
-      props.onChange({ target: { value: "" } });
-    }
-    return (
-      <div>
-        <input {...props} className="d-none" />
-        <AppIcon name="calendar" className="hand" onClick={openCalendar} />
-        {props.value && (
-          <AppIcon name="calendar-times-o" className="hand" onClick={clear} />
-        )}
-      </div>
-    );
-  };
 
   const displayDueDays = (due) => {
     if (due === null) return null;
@@ -104,20 +83,6 @@ const AppTaskListItemDeadline = ({
     <div>
       {displayDueIcon(due)}
       {displayDueDays(due)}
-      {onTaskDeadlineChange && (
-        <div className="taskDueEdit">
-          <Datetime
-            closeOnClickOutside={true}
-            closeOnSelect={false}
-            onChange={(selectedDate) => {
-              onTaskDeadlineChange &&
-                onTaskDeadlineChange(taskId, selectedDate);
-            }}
-            renderInput={renderTaskDeadlineCalendar}
-            value={deadline ? new Date(deadline).getTime() : null}
-          />
-        </div>
-      )}
     </div>
   );
 };
